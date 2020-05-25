0faa193e2e5f05640fdcc950ed3fa754f23ed119
Richard Duque <richy9.13@gmail.com>
2020-05-25 00:34:25 -0500

    modularizo el cart

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,6 @@ import { FormsModule } from '@angular/forms';
 import { CoreModule } from './core/core.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CartComponent } from './cart/cart.component';
 import { DemoComponent } from './demo/demo.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { LayoutComponent } from './layout/layout.component';
@@ -14,7 +13,6 @@ import { SharedModule } from './shared/shared.module';
 @NgModule({
   declarations: [
     AppComponent,
-    CartComponent,
     DemoComponent,
     PageNotFoundComponent,
     LayoutComponent
